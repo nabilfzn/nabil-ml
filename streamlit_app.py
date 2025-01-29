e673e95f5ef803e330c5fe9cb693448f0f0367bd
nabilfzn <140621593+nabilfzn@users.noreply.github.com>
2025-01-29 21:17:20 +0700

    comit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title('🐱‍👤 ML Data Penjualan')
 
-st.write('Hallo semuanya saya Nabil, disini saya menyediakan sebuah web app untuk demo melihat perkembangan suatu perusahaan melalui data penjualannya dari waktu ke waktu ')
+st.write('''
+Hallo semuanya saya Nabil, disini saya menyediakan 
+sebuah web app untuk demo melihat perkembangan suatu perusahaan
+melalui data penjualannya dari waktu ke waktu
+ ''')
